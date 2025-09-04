12de9ec5844e6f968153713bfc34ea26a01a3a88
testingworldjs <jitaaksh70@gmail.com>
2025-09-04 10:28:20 -0700

    updated

---
+++ question1.py
@@ -0,0 +1,8 @@
+class Solution:
+    def printNumber(self):
+        num = input("Enter the number: ")  # take input from user
+        print("You entered:", num)
+
+# Example usage
+s = Solution()
+s.printNumber()
